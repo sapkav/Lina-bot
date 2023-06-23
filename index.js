066c9f5645f8a445b33f47e9cbec2e4275fc60f2
sapkav <saipashev_vanya@mail.ru>
2023-06-23 13:45:54 +0300

    arr of compliments

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const telegramApi = require('node-telegram-bot-api');
-const axios = require('axios');
+// const telegramApi = require('node-telegram-bot-api');
+// const axios = require('axios');
+import telegramApi from 'node-telegram-bot-api';
+import axios from 'axios';
+import obj from '../tg bo/token.js';
 
-process.env["NTBA_FIX_350"] = 1
+process.env["NTBA_FIX_350"] = 1;
 
 const token = '6080535377:AAEjS9BpyEHCNISq-D-r2wPY3aWSkesR4IE';
-
-const stickersObj = {
-  favor: 'CAACAgIAAxkBAAEJa6ZkktNzA7zMVxZ55yU_-cc2agy0MgACEyQAAtn6YUjRlmdm6LR15C8E'
-}
 
 const imgObj = {
     kiss: '../loveBot/img/kiss.jpg',
 }
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const bot = new telegramApi(token, {polling: true});
-let chatId = 937439730;
+let chatId;
 
 setInterval(async () => {
   const date = new Date();
   // K = C + 273,15.
-  if (date.getHours() === 12 && date.getMinutes() === 22) {
+  if (date.getHours() === 6 && date.getMinutes() === 0) {
     await bot.sendMessage(chatId, `Доброе утро`);
-    await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Zelenograd,ru&APPID=eb38728c789610266c28269ce27507b5')
+    await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Zelenograd,ru&APPID=${obj.tokenWeather}`)
     .then(function (response) {
-      bot.sendMessage(msg.chat.id, `Погода в Зеленограде сейчас ${Math.round(response.data.main.temp - 273.15)}°C. Чувствуется как ${Math.round(response.data.main.feels_like - 273.15)}°C. Скорость ветра ${response.data.wind.speed} м/c. Влажность ${response.data.main.humidity}%`);
+      bot.sendMessage(chatId, `Погода в Зеленограде сейчас ${Math.round(response.data.main.temp - 273.15)}°C. Чувствуется как ${Math.round(response.data.main.feels_like - 273.15)}°C. Скорость ветра ${response.data.wind.speed} м/c. Влажность ${response.data.main.humidity}%`);
     })
   }
 }, 60000)
@@ -32,39 +37,43 @@ bot.setMyCommands([
     {command: '/love', description: 'Признание в любви'},
     {command: '/compliment', description: 'Комплимент для любимой'},
     {command: '/start', description: 'Старт'},
+    {command: '/kiss', description: 'Поцелуй'},
 ])
 
 bot.on('message', async msg => {
     const text = msg.text;
+    chatId = msg.chat.id;
     if (!text) return
+
     if (text === '/start') {
-      chatId = msg.chat.id;
       await bot.sendMessage(msg.chat.id, `Солнышко, этот чат для тебя, тут и будут комплименты, но пока я его разрабатываю :)`);
       return;
     }
+
     if (text === '/love') {
-        chatId = msg.chat.id;
         await bot.sendMessage(msg.chat.id, `Я тебя очень сильно люблю`);
-        await bot.sendSticker(msg.chat.id, stickersObj.favor)
+        await bot.sendSticker(msg.chat.id, obj.stickersArr[0])
         return;
     }
+
     if (text === '/compliment') {
-        chatId = msg.chat.id;
-        await bot.sendMessage(msg.chat.id, `Ты очень красивая`);
+        await bot.sendMessage(msg.chat.id, obj.compliments[getRandomInt(0, obj.compliments.length - 1)]);
+        await bot.sendSticker(msg.chat.id, obj.stickersArr[getRandomInt(0, obj.stickersArr.length - 1)])
         return;
+    }
+
+    if (text === '/kiss') {
+      await bot.sendMessage(msg.chat.id, 'Целую');
+      await bot.sendPhoto(msg.chat.id, imgObj.kiss)
+      return;
     }
 
     if (text.slice(0, 5).toLowerCase() === 'люблю') {
       await bot.sendMessage(msg.chat.id, `И я тебя очень сильно`);
       return;
     }
-    // await bot.sendPhoto(msg.chat.id, imgObj.kiss)
-    // await bot.sendMessage(msg.chat.id, arr);
-    // await bot.sendSticker(msg.chat.id, stickersObj.favor)
-    await axios.get('http://api.openweathermap.org/data/2.5/weather?q=Zelenograd,ru&APPID=eb38728c789610266c28269ce27507b5')
-    .then(function (response) {
-      bot.sendMessage(msg.chat.id, `Погода в Зеленограде сейчас ${Math.round(response.data.main.temp - 273.15)}°C. Чувствуется как ${Math.round(response.data.main.feels_like - 273.15)}°C. Скорость ветра ${response.data.wind.speed} м/c. Влажность ${response.data.main.humidity}%`);
-    })
+
     return;
 })
+
 
