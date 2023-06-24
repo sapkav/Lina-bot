4b41fc3b28954e2d93f5e77e888322059feaed14
sapkav <saipashev_vanya@mail.ru>
2023-06-24 13:26:11 +0300

    final mb

---
+++ token.js
@@ -0,0 +1,15 @@
+const obj = {
+    tokenWeather: 'eb38728c789610266c28269ce27507b5',
+    tgToken: '6080535377:AAEjS9BpyEHCNISq-D-r2wPY3aWSkesR4IE',
+    compliments: ['Ты очень красивая, зайка моя', 'Изумительная, любовь моя', 'Очень хочу тебя обнять, любовь моя', 'Ты самая лучшая девушка', 'Твоя фигура изумительная, зайка', 'Ты прекрасная',
+    'Ты безумно красивая', 'Ты самая сексуальная девушка в мире, очень хочу тебя поцеловать', 'Я безумно счастлив с тобой', 'Хочу исцеловать всю тебя', 'Ты превосходна, любовь моя',
+    'Безумно хочу тебя обнять', 'Ты безумно сексуальная', 'Зай, ты изумительна', 'Самая лучшая'],
+    nameArr: ['Любовь моя', 'Солнышко', 'Зайка', 'Золотце', 'Милашка'],
+    stickersArr: ['CAACAgIAAxkBAAEJa6ZkktNzA7zMVxZ55yU_-cc2agy0MgACEyQAAtn6YUjRlmdm6LR15C8E',
+    'https://tlgrm.ru/_/stickers/a72/8f0/a728f025-583c-3c0b-b210-d34979282f49/7.webp',
+    'https://tlgrm.ru/_/stickers/a72/8f0/a728f025-583c-3c0b-b210-d34979282f49/1.webp',
+    'https://tlgrm.ru/_/stickers/a72/8f0/a728f025-583c-3c0b-b210-d34979282f49/5.webp',
+    'https://tlgrm.ru/_/stickers/949/e1e/949e1e4f-48aa-4a2b-9c22-640475eee1dd/1.webp',
+    'https://tlgrm.ru/_/stickers/5cc/7a5/5cc7a5e5-1c4d-4eb5-8ea2-94a23e4142a9/1.webp']
+}
+export default obj;
